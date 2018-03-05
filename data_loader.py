6776cddae30af467eaf85351ed5c919f069cc35b
oMJACo <31996006+oMJACo@users.noreply.github.com>
2018-03-05 13:05:33 +0000

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -8,43 +8,75 @@ Created on Fri Feb  2 11:12:10 2018
 
 from torchtext import data
 from torchtext import datasets
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
 
 # Approach 1:
 # set up fields
-TEXT = data.Field(lower=True, batch_first=True)
+TEXT = data.Field(lower=False, batch_first=True, eos_token="<eos>")
+TGT  = data.Field(lower=False, batch_first=True, eos_token="<eos>")
 
 # make splits for data
-train, valid = datasets.LanguageModelingDataset.splits(path='/Users/murraycowan/Desktop/Uni Stuff/Fourth Year/Dissertation/folk-rnn-master/data/allabcwrepeats_parsed', train='_train', 
-                                                       validation='_valid',
-                                                text_field=TEXT)
+train = datasets.MusicDataset(path='/home/mcowan/Dissertation/single_line_data/',
+                              exts=('train_src', 'train_tgt'), 
+                              fields=[('src',TEXT),('trg',TGT)])
 
-import pdb
-pdb.set_trace()
-
-# print information about the data
-print('train.fields', train.fields)
-print('len(train)', len(train))
-print('vars(train[0])', vars(train[0])['text'][0:10])
+valid = datasets.MusicDataset(path='/home/mcowan/Dissertation/single_line_data/',
+                              exts=('valid_src', 'valid_tgt'),
+                              fields=[('src',TEXT),('trg', TGT)])
 
 # build the vocabulary
-TEXT.build_vocab(train, vectors=GloVe(name='6B', dim=300))
+TEXT.build_vocab(train)
+TGT.build_vocab(train)
 
 # print vocab information
 print('len(TEXT.vocab)', len(TEXT.vocab))
+print('len(TGT.vocab)', len(TGT.vocab))
 
 # make iterator for splits
 train_iter, valid_iter = data.BucketIterator.splits(
-    (train, valid), batch_size=3, bptt_len=30, device=0)
+    (train, valid), batch_size=(500), device=-1)
 
-# print batch information
-batch = next(iter(train_iter))
-print(batch.text)
-print(batch.target)
+train_batch = next(iter(train_iter))
+valid_batch = next(iter(valid_iter))
+#can use batch.src, batch.trg
 
-# Approach 2:
-train_iter, valid_iter = datasets.WikiText2.iters(batch_size=4, bptt_len=30)
+EMBEDDING_DIM = 6
+HIDDEN_DIM = 6
 
-# print batch information
-batch = next(iter(train_iter))
-print(batch.text)
-print(batch.target)
+class LSTM(nn.Module):
+
+    def __init__(self, embedding_dim, hidden_dim, src_vocab_size, trg_vocab_size):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        self.word_embeddings = nn.Embedding(src_vocab_size, embedding_dim)
+
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+
+        self.hidden2trg = nn.Linear(hidden_dim, trg_vocab_size)
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        return (autograd.Variable(torch.zeros(1,1,self.hidden_dim)),
+                autograd.Variable(torch.zeros(1,1,self.hidden_dim)))
+
+    def forward(self, tune):
+        embeds = self.word_embeddings(tune)
+        lst_out, self.hidden = self.lstm(
+                embeds.view(len(tune), 1, -1), self.hidden)
+        trg_space = self.hidden2trg(lstm.out.view(len(tune), -1))
+        trg_scores = F.log_softmax(trg_space)
+
+#Train Model
+
+model = LSTM(EMBEDDING_DIM, HIDDEN_DIM, len(TEXT.vocab), len(TGT.vocab))
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr = 0.1)
+
+trg_scores = model()
+print(tag_scores)
+
